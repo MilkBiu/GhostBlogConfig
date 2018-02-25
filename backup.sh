5a05a371936702cc5dfc11c66d2f1adf2ba4a5bb
YogurtBiu <1129367355@qq.com>
2018-02-25 16:33:00 +0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
-LOCAL_BAK_DIR="/root/backup"  #备份文件到指定位置
-MYSQL_SERVER="localhost"      #MySQL 服务器的地址，一般是本地地址
-MYSQL_USER="mysqluser"        #MySQL 的用户名名称，一般是 root 或 admin
-MYSQL_PASS="mysqlpassword"    #MySQL 用户的密码
+LOCAL_BAK_DIR="/root/backup"  #备份文件到指定位置 
+MYSQL_SERVER="localhost"      #MySQL 服务器的地址，一般是本地地址 
+MYSQL_USER="mysqluser"        #MySQL 的用户名名称，一般是 root 或 admin 
+MYSQL_PASS="mysqlpassword"    #MySQL 用户的密码 
 
 echo "Start Backup"
 
-# 备份并打包 MySQL 数据库
+# 备份并打包 MySQL 数据库 
 mysqldump -u $MYSQL_USER -h $MYSQL_SERVER -p $MYSQL_PASS --all-databases > /tmp/dbbak.sql
 tar zcvf $LOCAL_BAK_DIR/MySQL.tar.gz -P /tmp/dbbak.sql
 rm -f /tmp/dbbak.sql
 echo "Database backup success..."
 
-# 备份并打包网站文件
+# 备份并打包网站文件 
 tar zcfP $LOCAL_BAK_DIR/web.tar.gz /var/www/
 tar zcfP $LOCAL_BAK_DIR/nginx.tar.gz /etc/nginx/sites-available
-#如果安装的是 LNMP 一键包则备份 /usr/local/nginx/conf /home/wwwroot
+#如果安装的是 LNMP 一键包则备份 /usr/local/nginx/conf /home/wwwroot 
 echo "Web files backup success..."
 
-# 备份结束
+# 备份结束 
 echo "Backup Done! "$(date +"%Y.%m.%d") >> /root/backup.log
